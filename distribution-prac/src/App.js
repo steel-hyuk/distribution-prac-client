054286cfa8db0dd0b89535955e5f48a919bd2b53
steel-hyuk <skypeed3@naver.com>
2021-10-04 21:15:12 +0900

    commit

---
+++ distribution-prac/src/App.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React, { useState, useCallback } from 'react';
+
+import './App.css';
+
+function App() {
+  const [text, setText] = useState('');
+
+  const onChangeText = useCallback((event) => {
+    setText(event.target.value);
+  }, []);
+
+  const submitText = async () => {
+    await axios.post('http://ec2-15-164-171-108.ap-northeast-2.compute.amazonaws.com/test', {
+      text: text
+    }, {
+      'Content-Type': 'application/json'
+    })
+    .then((res) => {
+      console.log('res: ', res);
+      // console.log('api: ', api);
+    })
+  }
+
+  return (
+    <div className="App">
+      <input type='text' placeholder='입력' value={text} onChange={onChangeText} />
+      <button onClick={submitText}>전송</button>
+    </div>
+  );
+}
+
+export default App;
